53b296bfa4dbd936d8b31537e43d4d3c8e0df88a
Mike Coletta <coletta.mike@gmail.com>
2021-06-18 18:48:29 -0700

    Refactored navbar to react-bootstrap

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,37 +1,35 @@
 import { Link } from "react-router-dom"
 import React from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import "./style.css";
 
-function Navbar() {
+function CustomNavbar() {
     return (
-<nav className="navbar navbar-dark sticky-top navbar-expand-md">
-<div className="container-fluid">
-    <Link to="/" className="navbar-brand">
-        <h1 className="navbar-name">Mike Coletta</h1>
-    </Link>
-    <button className="navbar-toggler dropdownButton" type="button" data-toggle="collapse" data-target="#portfolio-navbar"
-        aria-controls="portfolio-navbar" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon dropdownIcon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="portfolio-navbar">
-        <ul className="navbar-nav ml-auto">
-            <li className="dropdown-item">
-                <Link to="/about" className="navlink">About</Link>
-            </li>
-            <li className="dropdown-item">
-                <Link to="/media" className="navlink">Media</Link>
-            </li>
-            <li className="dropdown-item">
-                <Link to="/podcasts" className="navlink">Podcasts</Link>
-            </li>
-            <li className="dropdown-item">
-                <Link to="/contact" className="navlink">Contact</Link>
-            </li>
-        </ul>
-    </div>
-</div>
-</nav>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="#home">
+                <Link to="/" className="navbar-brand">
+                    <h1 className="navbar-name">Mike Coletta</h1>
+                </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link href="#features">
+                        <Link to="/about" className="navlink">About</Link>
+                    </Nav.Link>
+                    <Nav.Link href="#pricing">
+                        <Link to="/media" className="navlink">Media</Link>
+                    </Nav.Link>
+                    <Nav.Link href="#pricing">
+                        <Link to="/podcasts" className="navlink">Podcasts</Link>
+                    </Nav.Link>
+                    <Nav.Link href="#pricing"><Link to="/media" className="navlink">
+                        <Link to="/contact" className="navlink">Contact</Link>
+                    </Link></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
-export default Navbar;
+export default CustomNavbar;
