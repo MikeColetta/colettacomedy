f595a6e9d588b222d5d8742caea3da826dcfc53b
Mike Coletta <coletta.mike@gmail.com>
2021-06-18 19:12:44 -0700

    Added navlink buttons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,37 +1,46 @@
 import { Link } from "react-router-dom"
 import React from "react";
-import { Navbar, Nav, Row, Container, Card} from "react-bootstrap";
+import { Navbar, Nav, Row, Container, Card, Button } from "react-bootstrap";
 import "./style.css";
 
 function CustomNavbar() {
     return (
         <Container>
-        <Card className="header">
-            <Row className="headerRow">
-                <Link to="/">
-                    <h1 className="navbar-name">Home of Comedian Mike Coletta</h1>
-                </Link>
-            </Row>
-            <Navbar collapseOnSelect expand="lg" className="customNavbar">
+            <Card className="header">
+                <Row className="headerRow">
+                    <Link to="/">
+                        <h1 className="headerName">Home of Comedian Mike Coletta</h1>
+                    </Link>
+                </Row>
+                <Navbar collapseOnSelect expand="lg" className="customNavbar">
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="m-auto">
+
                             <Nav.Link href="#features">
-                                <Link to="/about" className="navlink">About</Link>
+                                <Button className='navLinkButton'>
+                                    <Link to="/about" className="navlink">About</Link>
+                                </Button>
                             </Nav.Link>
                             <Nav.Link href="#pricing">
-                                <Link to="/media" className="navlink">Media</Link>
+                                <Button className='navLinkButton'>
+                                    <Link to="/media" className="navlink">Media</Link>
+                                </Button>
                             </Nav.Link>
                             <Nav.Link href="#pricing">
-                                <Link to="/podcasts" className="navlink">Podcasts</Link>
+                                <Button className='navLinkButton'>
+                                    <Link to="/podcasts" className="navlink">Podcasts</Link>
+                                </Button>
                             </Nav.Link>
                             <Nav.Link href="#pricing"><Link to="/media" className="navlink">
-                                <Link to="/contact" className="navlink">Contact</Link>
+                                <Button className='navLinkButton'>
+                                    <Link to="/contact" className="navlink">Contact</Link>
+                                </Button>
                             </Link></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
-            </Navbar>
-        </Card>
+                </Navbar>
+            </Card>
         </Container>
     )
 }
