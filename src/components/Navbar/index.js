4a624caebd6cb65eaca962c3fda1561413fd5921
Mike Coletta <coletta.mike@gmail.com>
2021-06-21 19:32:54 -0700

    Fixed Navbar buttons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,36 +8,45 @@ function CustomNavbar() {
         <Container>
             <Card className="header">
                 <Row className="mt-2 headerRow">
-                    <Button className="navLinkButton">
-                        <Link to="/">
+                    <Link to="/" className="navlink">
+                        <Button className="navLinkButton">
                             <h1 className="headerName">Home of Comedian Mike Coletta</h1>
-                        </Link>
-                    </Button>
+                        </Button>
+                    </Link>
                 </Row>
                 <Navbar collapseOnSelect expand="lg" className="customNavbar">
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="m-auto">
-                            <Nav.Link href="#features">
-                                <Button className='navLinkButton'>
-                                    <Link to="/about" className="navlink">About</Link>
-                                </Button>
+                            <Nav.Link>
+                                <Link to="/about" className="navlink">
+                                    <Button className='navLinkButton'>
+                                        <h5>About</h5>
+                                    </Button>
+                                </Link>
                             </Nav.Link>
-                            <Nav.Link href="#pricing">
-                                <Button className='navLinkButton'>
-                                    <Link to="/media" className="navlink">Media</Link>
-                                </Button>
+                            <Nav.Link>
+                                <Link to="/media" className="navlink">
+                                    <Button className='navLinkButton'>
+                                        <h5>Media</h5>
+                                    </Button>
+                                </Link>
                             </Nav.Link>
-                            <Nav.Link href="#pricing">
-                                <Button className='navLinkButton'>
-                                    <Link to="/podcasts" className="navlink">Podcasts</Link>
-                                </Button>
+                            <Nav.Link>
+                                <Link to="/podcasts" className="navlink">
+                                    <Button className='navLinkButton'>
+                                        <h5>Podcasts</h5>
+                                    </Button>
+                                </Link>
                             </Nav.Link>
-                            <Nav.Link href="#pricing"><Link to="/media" className="navlink">
-                                <Button className='navLinkButton'>
-                                    <Link to="/contact" className="navlink">Contact</Link>
-                                </Button>
-                            </Link></Nav.Link>
+                            <Nav.Link>
+
+                                <Link to="/contact" className="navlink">
+                                    <Button className='navLinkButton'>
+                                        <h5>Contact</h5>
+                                    </Button>
+                                </Link>
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
