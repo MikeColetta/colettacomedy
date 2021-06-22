012a6c2788b8a8915f7f63bb741fc2cde2d2787e
Mike Coletta <coletta.mike@gmail.com>
2021-06-21 19:19:21 -0700

    Fixed footer error for screenreaders

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,9 +5,9 @@ function Footer() {
     return (
     <footer className="footer footer-style">
         <div className="inline">
-            <a href="https://www.facebook.com/coletta.mike/" className="fa fa-facebook" target="_blank" rel="noreferrer"></a>
-            <a href="https://twitter.com/MeColetta" className="fa fa-twitter" target="_blank" rel="noreferrer"></a>
-            <a href="https://www.instagram.com/mecoletta/" className="fa fa-instagram" target="_blank" rel="noreferrer"></a>
+            <a href="https://www.facebook.com/coletta.mike/" className="fa fa-facebook" target="_blank" rel="noreferrer" aria-hidden="true"> </a>
+            <a href="https://twitter.com/MeColetta" className="fa fa-twitter" target="_blank" rel="noreferrer" aria-hidden="true"> </a>
+            <a href="https://www.instagram.com/mecoletta/" className="fa fa-instagram" target="_blank" rel="noreferrer" aria-hidden="true"> </a>
             <p className="copyright">© Copyright 2021 Michael Coletta</p>
         </div>
     </footer>
