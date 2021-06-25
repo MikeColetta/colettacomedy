403869979d6d3db3cbd3069bb56fc734c013f032
Mike Coletta <coletta.mike@gmail.com>
2021-06-24 17:23:22 -0700

    Contact form working with EmailJS

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,23 +1,16 @@
 import "./style.css";
+import ContactForm from "../../components/ContactForm";
 
 function Contact() {
     return (
         <div className="container my-2" id="contact">
-        <div className="card contactCard">
-            <div className="card-header contactCardHeader">
-                <h2>Contact</h2>
-            </div>
-            <div>
-            <h3 className="card-body contactSubheading">Send me an email at coletta.mike@gmail.com!</h3>            </div>
-            <div className="row iconRow">
-                <div className="card col-6 iconCard">
-                    <a href="mailto:coletta.mike@gmail.com" target="_blank" rel="noreferrer"><span
-                            className="fa fa-envelope-o fa-5x"></span></a>
+            <div className="card contactCard">
+                <div className="card-header contactCardHeader">
+                    <h2>Contact</h2>
                 </div>
+                <ContactForm />
             </div>
-            
         </div>
-    </div>
     )
 }
 
